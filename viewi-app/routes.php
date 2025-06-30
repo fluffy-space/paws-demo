937ed9ba7283210a386621fd5609efd5f4463916
Ivan Voitovych <col606v@gmail.com>
2025-06-30 10:56:41 +0000

    router

--- viewi-app/routes.php
+++ viewi-app/routes.php
@@ -9,9 +9,4 @@ use Viewi\Components\Http\Message\Response;
  * @var App $app
  */
 $router = $app->router();
-$router->get('/', HomePage::class);
-$router
-    ->get('*', NotFoundPage::class)
-    ->transform(function (Response $response) {
-        return $response->withStatus(404, 'Not Found');
-    });
+
